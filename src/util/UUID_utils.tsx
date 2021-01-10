299a5d115b36410d3b7c2701a17c02e30da2cbf1
admin <admin@huelite.in>
2021-01-10 06:56:03 +0530

    ## @types/huelite
    ## changeLog
    - `util/UUID` added `generate_UUID_10_withVenderPrefix` for genreating 10char UUID with a venderPrefix
    ---
    
    @HUElite EXPO APP
    
    ---

--- src/util/UUID_utils.tsx
+++ src/util/UUID_utils.tsx
@@ -1,7 +1,16 @@
+import UNIVERSALS from "../@types"
 import { reduxStore } from "../redux";
 
 export function uuidv4() {
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+export function generate_UUID_10_withVenderPrefix() {
+  return UNIVERSALS.venderConf.venderPrefix + "xxxxxxxxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
       v = c == "x" ? r : (r & 0x3) | 0x8;
     return v.toString(16);
@@ -17,6 +26,7 @@ export function generateRandomUserId() {
   });
 }
 
+/** @deprecated */
 export function generateUUID() {
   return "xxxxxxxx-xxxx-xxxx".replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
@@ -25,6 +35,7 @@ export function generateUUID() {
   });
 }
 
+/** @deprecated */
 export const getContainerUUID = () => {
   let UUID = generateUUID()
   while (checkUUIDMatch(UUID)) {
@@ -33,6 +44,7 @@ export const getContainerUUID = () => {
   return UUID
 }
 
+/** @deprecated */
 const checkUUIDMatch = (UUID: string) => {
   reduxStore.store.getState().deviceReducer.containers.forEach((container, index) => {
     if (UUID == container.conUUID)
