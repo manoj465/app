06475e1a23ec8cd12f7e7eb34ccc3cf477b426d1
admin <admin@huelite.in>
2021-01-10 06:20:38 +0530

    ## @types/huelite - updates HUE_TIMER   - introduces universal enum for daytime, eventype, timer days array object, and timer status. All with `enum 0` as undefined for validation check if required. however it can be trusted blindly
    ## changeLog
    - added `daytime` `mng` icon in timer editor modal header

--- src/@types/huelite/timer/types.ts
+++ src/@types/huelite/timer/types.ts
@@ -1,15 +1,35 @@
 
 export type HUE_TIMER_group_t = HUE_TIMER_t & { devices: string[] }
 
+export enum HUE_TIMER_EVENT_TYPE_e {
+    undefined,
+    ON,
+    OFF
+}
+
+export enum HUE_TIMER_DAYTIME_e {
+    undefined,
+    AM,
+    PM
+}
+
+export enum HUE_TIMER_STATUS_e {
+    undefined,
+    INACTIVE,
+    ONCE,
+    REPEAT
+}
+
+export type HUE_TIMER_DAYS_t = [boolean, boolean, boolean, boolean, boolean, boolean, boolean]
+
 /** @description >- Client side Local representation of timer Object */
 export interface HUE_TIMER_t {
     id: string,
     H: number,
     M: number,
-    DT: number,
-    ET: number,
-    STATUS: number,
-    DAYS: [boolean, boolean, boolean, boolean, boolean, boolean, boolean],
+    DT: HUE_TIMER_DAYTIME_e,
+    ET: HUE_TIMER_EVENT_TYPE_e,
+    STATUS: HUE_TIMER_STATUS_e,
+    DAYS: HUE_TIMER_DAYS_t,
 }
 
-/** test-001 */
