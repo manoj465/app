8ab5b6ae662214b1ef7130119dfa446df730cb89
admin <admin@huelite.in>
2021-01-18 17:04:46 +0530

    Updates

---
+++ precache-manifest.be7211046ef8c4a9cebb001fbca1e99f.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "346976934a831cf742e018ab174810b3",
+    "url": "/HUElite_App_Expo/static/media/deepocean.0aee9a72.png"
+  },
+  {
+    "revision": "1344e9a0633f5feb2afb",
+    "url": "/HUElite_App_Expo/static/js/app.af55ad95.chunk.js"
+  },
+  {
+    "revision": "6e008895cc75c89d54f0460c9502233e",
+    "url": "/HUElite_App_Expo/static/media/usageMeter.42872b19.png"
+  },
+  {
+    "revision": "2ca5be296d900971bb32f3726a2e647a",
+    "url": "/HUElite_App_Expo/static/media/testIMG.922f6a75.jpg"
+  },
+  {
+    "revision": "588cd85ff0450a1d7ebf09abeee78459",
+    "url": "/HUElite_App_Expo/static/media/winter.cdc1f980.png"
+  },
+  {
+    "revision": "dc0a8793995406553d6c1417191a4c7f",
+    "url": "/HUElite_App_Expo/static/media/sunset.8780ce1b.png"
+  },
+  {
+    "revision": "cba28a0dca20f5375449c53a6a0d8e56",
+    "url": "/HUElite_App_Expo/static/media/sunrise.a239aa8e.png"
+  },
+  {
+    "revision": "1d82c0a284e7e9107ec750546bf6ff90",
+    "url": "/HUElite_App_Expo/static/media/spring1.f58006c7.png"
+  },
+  {
+    "revision": "d73272f6fc10a91dff13",
+    "url": "/HUElite_App_Expo/static/js/runtime~app.0b889b6d.js"
+  },
+  {
+    "revision": "cf7ac762ea8be26546f2c8721991552e",
+    "url": "/HUElite_App_Expo/static/media/autumn.c72bcc06.png"
+  },
+  {
+    "revision": "ea7c735dff7f720f32d58d5a64290edd",
+    "url": "/HUElite_App_Expo/static/media/aurora.d7429eea.png"
+  },
+  {
+    "revision": "10471d5b987caea2a8ee8b8d97a709d0",
+    "url": "/HUElite_App_Expo/static/media/splash-icon.642f417a.png"
+  },
+  {
+    "revision": "cabc1f84bebe52d5307f767f6b4cffc8",
+    "url": "/HUElite_App_Expo/static/media/background.76e3813d.jpg"
+  },
+  {
+    "revision": "ddcfd0b20eb2f139bb2c519f27399ceb",
+    "url": "/HUElite_App_Expo/static/media/1.0b297109.png"
+  },
+  {
+    "revision": "6a74a67f277866913578733da56f20dc",
+    "url": "/HUElite_App_Expo/static/media/colorPicker.116187dc.png"
+  },
+  {
+    "revision": "9d1d0338ab900f07a15b1122998d11e0",
+    "url": "/HUElite_App_Expo/static/media/preset.696f2cd2.png"
+  },
+  {
+    "revision": "6f9458d7f742c379ccfda1f816a83496",
+    "url": "/HUElite_App_Expo/static/media/setting.97214bba.png"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/HUElite_App_Expo/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "14cd07695b3fc46e83c82540072662a7",
+    "url": "/HUElite_App_Expo/manifest.json"
+  },
+  {
+    "revision": "4383575f587bdc07224770f7813ef928",
+    "url": "/HUElite_App_Expo/static/js/2.2ddef792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8230017bdd0f83ef5f9f7ebd414f7306",
+    "url": "/HUElite_App_Expo/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/HUElite_App_Expo/serve.json"
+  },
+  {
+    "revision": "6ae208e22cd8933f64e4",
+    "url": "/HUElite_App_Expo/static/js/2.2ddef792.chunk.js"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/HUElite_App_Expo/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/HUElite_App_Expo/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "8ef52a15e44481b41e7db3c7eaf9bb83",
+    "url": "/HUElite_App_Expo/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/HUElite_App_Expo/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/HUElite_App_Expo/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "6a2ddad1092a0a1c326b6d0e738e682b",
+    "url": "/HUElite_App_Expo/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/HUElite_App_Expo/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "751990387037aca63d59b639cf558e88",
+    "url": "/HUElite_App_Expo/index.html"
+  },
+  {
+    "revision": "b3263095df30cb7db78c613e73f9499a",
+    "url": "/HUElite_App_Expo/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/HUElite_App_Expo/expo-service-worker.js"
+  },
+  {
+    "revision": "0c947decef421bb316dce6218ca8e992",
+    "url": "/HUElite_App_Expo/favicon.ico"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/HUElite_App_Expo/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "fa11ce5e24fefeb3d0a67fce4770544f",
+    "url": "/HUElite_App_Expo/favicon-16.png"
+  },
+  {
+    "revision": "28fdcc74d32adc5dfedbbb1ace963b50",
+    "url": "/HUElite_App_Expo/favicon-32.png"
+  },
+  {
+    "revision": "e766963327e0a89f9ec2ba88646b6177",
+    "url": "/HUElite_App_Expo/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/HUElite_App_Expo/./fonts/AntDesign.ttf"
+  }
+];
