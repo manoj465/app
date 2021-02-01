72c6e7b597e8bcd31ae041dda32b7d9638d8dc63
admin <admin@huelite.in>
2021-02-02 03:29:30 +0530

    removed @operator to add it as subtree

--- src/@operator/index.ts
+++
@@ -1,6 +0,0 @@
-import user from "./user.operator"
-import device from "./device.operator"
-import { userStoreUpdateFunction } from "./user.operator"
-
-
-export const appOperator = { user, device, userStoreUpdateFunction }
